d6cfe8b747c61ba188c23c73334e607f25a1e528
hendralijaya <64987154+hendralijaya@users.noreply.github.com>
2023-02-23 12:12:16 +0700

    Adding Components in src

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// Parent Component
+import React, { useEffect, useState } from 'react';
+import Introduction from './components/introduction';
+import Profile from './components/profile';
 
 function App() {
+  const [ myFriend, setMyFriend ] = useState(1);
+  const [ myFriendName, setMyFriendName ] = useState('');
+  useEffect(()=> {
+    if(myFriend === 1) setMyFriendName('Budi')
+    else if(myFriend === 2) setMyFriendName('Joko')
+    else {
+      setMyFriendName('Joni')
+    }
+  }, [myFriend])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h5>My friend name is {myFriendName}</h5>
+      <h1> I have {myFriend} friend</h1>
+      <button onClick={() => setMyFriend((prev) => prev + 1)} disabled={myFriend >= 10}>Add Friend</button>
+      <button onClick={() => setMyFriend((prev) => prev - 1)} disabled={myFriend <= 0}>Remove Friend</button>
+      {/* make a validation if friend less than zero cant press the button */}
+      <Profile />
+      <Introduction name="hendra lijaya"/>
+      <Introduction name="jimmy lijaya"/>
     </div>
   );
 }
